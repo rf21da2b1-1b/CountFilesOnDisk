9f9df0bdf13b4b7af7cb471f3530aab8b7df0701
Peter Levinsky <pele@easj.dk>
2022-03-07 11:20:24 +0100

    Commets added

--- CountFilesOnDisk/AWorker.cs
+++ CountFilesOnDisk/AWorker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
@@ -17,44 +18,62 @@ namespace CountFilesOnDisk
 
         public void Start()
         {
+            // Start timer
             Stopwatch time = new Stopwatch();
             time.Start();
-            
-            string root = Directory.GetDirectoryRoot(@"\");
+
+            /*
+             * Find the root directory
+             */
+            string root = Directory.GetDirectoryRoot(@"C:\");
+            // find all directories in the root
             var dics = Directory.GetDirectories(root);
             foreach (string dic in dics)
             {
-                Scan(dic);
+                Scan(dic); // scan each sub directory
             }
 
+
+            // stop timer and print out result
             time.Stop();
             TimeSpan timeUsed = time.Elapsed;
             Console.WriteLine();
-            Console.WriteLine($"Used time is {timeUsed}");
+            Console.WriteLine($"Used time is {timeUsed.Minutes} min {timeUsed.Seconds} sec and {timeUsed.Milliseconds} msec" );
 
             Console.WriteLine("Number of files is " + noOfFiles);
 
         }
 
+        /// <summary>
+        /// Count the number of files in this directory,
+        /// and make a recursive call for subdirectories in the directory
+        /// </summary>
+        /// <param name="path">The path to the directory</param>
         public void Scan(String path)
         {
+            // show a dot for approx each 10000 dictionary - to show System status (Jacob Nielsen heuristic)
             counter++;
             if (counter % 10000 == 0) Console.Write('.');
 
             try
             {
+                // gets all subdirectories
                 var dics = Directory.GetDirectories(path);
 
                 foreach (string d in dics)
                 {
+                    // recursive call for each subdirectory
                     Scan(d);
                 }
 
+
+                // count the number of files in this directory (specified by the parameter) 
                 noOfFiles += Directory.GetFiles(path).Length;
+                
             }
             catch (Exception ex)
             {
-                // if unautorized acces to a file/dictionary i.e. not administrator
+                // if unauthorized access to a file/dictionary i.e. not administrator
                 // just skip
                 return;
             }
