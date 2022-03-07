f8ec9371d103104a0db5777569b9d9fbeebd0455
Peter Levinsky <pele@easj.dk>
2022-03-07 09:35:20 +0100

    Add project files.

---
+++ CountFilesOnDisk/AWorker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CountFilesOnDisk
+{
+    public class AWorker
+    {
+        private static int noOfFiles = 0;
+        private static int counter = 0;
+        
+        public AWorker()
+        {
+        }
+
+        public void Start()
+        {
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            
+            string root = Directory.GetDirectoryRoot(@"\");
+            var dics = Directory.GetDirectories(root);
+            foreach (string dic in dics)
+            {
+                Scan(dic);
+            }
+
+            time.Stop();
+            TimeSpan timeUsed = time.Elapsed;
+            Console.WriteLine();
+            Console.WriteLine($"Used time is {timeUsed}");
+
+            Console.WriteLine("Number of files is " + noOfFiles);
+
+        }
+
+        public void Scan(String path)
+        {
+            counter++;
+            if (counter % 10000 == 0) Console.Write('.');
+
+            try
+            {
+                var dics = Directory.GetDirectories(path);
+
+                foreach (string d in dics)
+                {
+                    Scan(d);
+                }
+
+                noOfFiles += Directory.GetFiles(path).Length;
+            }
+            catch (Exception ex)
+            {
+                // if unautorized acces to a file/dictionary i.e. not administrator
+                // just skip
+                return;
+            }
+        }
+    }
+}
---
+++ CountFilesOnDisk/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CountFilesOnDisk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AWorker worker = new AWorker();
+            worker.Start();
+
+            Console.Write("Strike a key : ");
+            Console.ReadKey();
+        }
+    }
+}
